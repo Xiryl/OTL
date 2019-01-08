b3e11508fb6eb1a0479a8130f4f49b8d81d4b828
Fabio Chiarani <fabio@chiarani.it>
2019-01-08 12:25:16 +0100

    [SERVER|UP] index for test mqtt library

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,42 @@
-/** File Name:  index.js
- *  Author: Chiarani Fabio
- *  Date: 6 GEN 2018
-*/
+const mqtt   = require('mqtt');
+
+const broker = 'mqtt://192.168.1.109';	// MQTT Broker hostname/IP address
+const client = mqtt.connect(broker);	// MQTT Client
+const device = 'sonoff-1';				// Sonoff device identifier
+
+let state = 'OFF';
+let timer;
+
+client.on('connect', function () {
+
+	console.log(`${Date.now()} Client connected to ${broker}`);
+
+	client.subscribe(`stat/${device}/+`);
+	client.subscribe(`tele/${device}/+`);
+
+	client.publish(`cmnd/${device}/status`);
+
+	timer = setInterval(loop, 2000);
+});
+
+client.on('message', function (topic, message) {
+
+	if (topic === `stat/${device}/POWER`) {
+		state = message.toString();
+	}
+
+	console.log(`${Date.now()} RX ${topic} ${message}`);
+});
+
+function loop() {
+
+	if (!client.connected) {
+		return timer && timer.clearInterval();
+	}
+
+	let newState = state === 'OFF' ? 'ON' : 'OFF';
+
+	client.publish(`cmnd/${device}/power`, newState);
+
+	console.log(`${Date.now()} TX cmnd/${device}/power ${newState}`);
+}
