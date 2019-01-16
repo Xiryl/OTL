1e25cd43e915e10624ff006ddf7005d71468f0f0
Fabio Chiarani <fabio@chiarani.it>
2019-01-16 08:32:15 +0100

    [CONTROLLER|UP] clean code

--- controller/controller.js
+++ controller/controller.js
@@ -1,24 +1,46 @@
-const mqtt   = require('mqtt');
+const mqtt		= require('mqtt');
+let express		= require('express')
+let app			= express();
 
-const broker = 'mqtt://192.168.43.110';	// MQTT Broker HOST
-const client = mqtt.connect(broker);	// MQTT Client
-const device = 'sonoff-1';				// SONOFF IDENTIFIER
+/** CONSTANTS */
+/**==================================================================================== */
+const SERVER_PORT = 8448;
+const BROKER_HOST = '192.168.43.110';
+const BROKER_ADDR = `mqtt://${BROKER_HOST}`;	
 
-let state = 'OFF';
+const allowed_devices = ['sonoff-1'];				// SONOFF IDENTIFIER
+
+/** API */
+/**==================================================================================== */
+app.get('/', (req, res) => {
+	
+	mqtt_client.publish(`cmnd/${device}/power`, 'OFF');	
+	res.send('Hello World with Express');
+
+});
+
+app.listen(SERVER_PORT, function () {
+     console.log(`[SERVER] Starting server on port ${SERVER_PORT} ...`);
+});
+
+
+/** MQTT */
+/**==================================================================================== */
+const mqtt_client = mqtt.connect(BROKER_ADDR);	
 
 /** CONNECTION HANDLER */
-client.on('connect', function () {
+mqtt_client.on('connect', function () {
 
 	console.log(`[CONTROLLER]-[connect]-[${Date.now()}] Client connected to ${broker}`);
 
 	/** subscribe to sonoff */
-	client.subscribe(`stat/${device}/+`);
-	client.publish	(`cmnd/${device}/status`);
+	mqtt_client.subscribe(`stat/${device}/+`);
+	mqtt_client.publish	(`cmnd/${device}/status`);
 
 });
 
-let first =  true;
-client.on('message', function (topic, message) {
+/** MESSAGE HANDLER */
+mqtt_client.on('message', function (topic, message) {
 
 	if( topic === `stat/${device}/STATUS`) {
 		console.log('[STATUS] inside status');
@@ -30,7 +52,7 @@ client.on('message', function (topic, message) {
 		console.log(`[STATUS] new power status : ${newStatus} [0] OFF - [1] ON`);
 		console.log('[STATUS] sending new power status ...');
 
-		client.publish(`cmnd/${device}/power`, newStatus);
+		//client.publish(`cmnd/${device}/power`, newStatus);
 		console.log('[STATUS] sended.');
 
 	}
@@ -45,6 +67,7 @@ client.on('message', function (topic, message) {
 
 		//** skip this */
 	}
+
 });
 
 
