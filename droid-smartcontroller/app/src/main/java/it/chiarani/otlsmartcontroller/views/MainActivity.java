08ddfc5d02279b4ac2b541c1a231ea960fd98ded
Xiryl <fabio@chiarani.it>
2019-03-19 07:29:32 +0100

    [ADD] room library

--- droid-smartcontroller/app/src/main/java/it/chiarani/otlsmartcontroller/views/MainActivity.java
+++ droid-smartcontroller/app/src/main/java/it/chiarani/otlsmartcontroller/views/MainActivity.java
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.mikhaellopez.circularimageview.CircularImageView;
 
 import java.security.Signature;
 
+import androidx.databinding.DataBindingUtil;
 import it.chiarani.otlsmartcontroller.R;
+import it.chiarani.otlsmartcontroller.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
 
 
-   // ActivityMainBinding binding;
+   ActivityMainBinding binding;
 
     @Override
     protected int getLayoutID() {
@@ -21,15 +28,24 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void setActivityBinding() {
-    //binding = DataBindingUtil.setContentView(this, getLayoutID());
+        binding = DataBindingUtil.setContentView(this, getLayoutID());
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Intent myIntent = new Intent(MainActivity.this, SignInActivity.class);
-       this.startActivity(myIntent);
+
+        String name = this.getIntent().getStringExtra("G-NAME");
+        String pic = this.getIntent().getStringExtra("G-PIC");
+        binding.activityMainWelcome.setText("Benvenuto "  + name.split(" ")[0]);
+
+
+
+        CircularImageView imageView = (CircularImageView) findViewById(R.id.img);
+        Glide.with(this).load(pic).into(imageView);
+
+
+      //  binding.img.setImageDrawable(new Dra);
     }
 }
--- droid-smartcontroller/app/src/main/java/it/chiarani/otlsmartcontroller/views/SignInActivity.java
+++ droid-smartcontroller/app/src/main/java/it/chiarani/otlsmartcontroller/views/SignInActivity.java
@@ -17,16 +17,24 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
-public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignInActivity extends BaseActivity implements View.OnClickListener {
 
     GoogleSignInClient mGoogleSignInClient;
     ActivitySignInBinding binding;
 
     @Override
+    protected int getLayoutID() {
+        return R.layout.activity_sign_in;
+    }
+
+    @Override
+    protected void setActivityBinding() {
+        binding = DataBindingUtil.setContentView(this, getLayoutID());
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
-      //  setContentView();
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -46,6 +54,21 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         // Check for existing Google Sign In account, if the user is already signed in
         // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account != null) {
+
+            /*GoogleSignInResult result =
+Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+GoogleSignInAccount acct = result.getSignInAccount();
+String personName = acct.getDisplayName();
+String personGivenName = acct.getGivenName();
+String personFamilyName = acct.getFamilyName();
+String personEmail = acct.getEmail();
+String personId = acct.getId();
+Uri personPhoto = acct.getPhotoUrl();*/
+            String personName = account.getDisplayName();
+            gotoMain(account);
+        }
+
         //  updateUI(account);
     }
 
@@ -81,9 +104,9 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
+            String personName = account.getDisplayName();
             // Signed in successfully, show authenticated UI.
-          //  updateUI(account);
-            int x = 1;
+            gotoMain(account);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
@@ -91,5 +114,13 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
             int x = 1;
            // updateUI(null);
         }
+    }
+
+    private void gotoMain(GoogleSignInAccount acc) {
+        Intent myIntent = new Intent(SignInActivity.this, MainActivity.class);
+        myIntent.putExtra("G-NAME", acc.getDisplayName());
+        String x  = acc.getPhotoUrl().toString();
+        myIntent.putExtra("G-PIC", x);
+        this.startActivity(myIntent);
     }
 }
